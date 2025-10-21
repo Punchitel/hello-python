ff6cd75459769d3634dcb600081f4c98e40d2c17
Tomáš Brestič <tomas.brestic@gmail.com>
2025-10-21 22:14:16 +0200

    Modify 'greet' function to return a string instead of printing output

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 import sys
 
-# vvv - modify this function to output greetings to the person called "name"
+# vvv - modify this function to return greetings to the person called "name"
 # e.g. if "name" is "Karel", the whole output should be "Hello, Karel!"
 def greet(name):
-    print("Hello... someone?")
+    return "Hello... someone?"
 # ^^^
 
 if len(sys.argv) > 1:
@@ -12,4 +12,4 @@ else:
     name = "world"
 
 if __name__ == "__main__":
-    greet(name)
+    print(greet(name))
